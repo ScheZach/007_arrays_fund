c0ab020ba1830a655626dccba4c1150ea090ace2
compsci <compsci@compsci>
2017-11-03 10:43:55 -0600

    Intial commit

---
+++ src/array_fund/matrix.java
@@ -0,0 +1,7 @@
+package array_fund;
+
+public class matrix {
+	//Matrix is another name for a multi-dimentsional array
+	//For example: a 2d array is often called a matrix
+	//2D like a checkerboard, chess board, ect.
+}
